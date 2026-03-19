b882f0813f56ddf3f802c4ce94ef8cfc828eb7ef
Justin Braun <justinbraun335@gmail.com>
2026-03-19 02:39:46 -0400

    chore: treat zsh files as bash

--- nvim/lua/core/filetypes.lua
+++ nvim/lua/core/filetypes.lua
@@ -5,5 +5,12 @@
 vim.filetype.add({
     extension = {
         sh = "bash",
+        zsh = "bash",
+    },
+    filename = {
+        [".zshrc"] = "bash",
+        [".zshenv"] = "bash",
+        [".zshenv.local"] = "bash",
+        [".zprofile"] = "bash",
     },
 })
