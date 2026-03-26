2491c9ee8caf66053e9294161d808bf26e6ee9be
Justin Braun <justinbraun335@gmail.com>
2026-03-26 06:39:39 -0400

    chore: add comment

--- nvim/lua/plugins/utils/cmdline.lua
+++ nvim/lua/plugins/utils/cmdline.lua
@@ -2,6 +2,9 @@
 -- =                                 CMDLINE.NVIM                                 =
 -- ================================================================================
 
+-- This plugin wipes out visual selections due to redraw
+-- https://github.com/neovim/neovim/issues/28510
+
 return {
     "jb49088/cmdline.nvim",
     -- enabled = false,
