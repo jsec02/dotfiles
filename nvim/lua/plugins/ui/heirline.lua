47bd81c1f0a624b5af4e1fe2120a84376de9707d
Justin Braun <justinbraun335@gmail.com>
2026-03-27 10:14:28 -0400

    chore: add event to diagnostic update

--- nvim/lua/plugins/ui/heirline.lua
+++ nvim/lua/plugins/ui/heirline.lua
@@ -287,7 +287,7 @@ return {
                 self.hints = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.HINT })
                 self.info = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.INFO })
             end,
-            update = { "DiagnosticChanged", "BufEnter" },
+            update = { "DiagnosticChanged", "BufEnter", "TextChanged" },
             {
                 provider = function(self)
                     if self.errors == 0 then
