2212ca409df67774f10cffe69266d2e9f4a9bbee
master <justinbraun335@gmail.com>
2026-06-19 15:38:12 -0400

    fix: fix outdated requires

--- nvim/lua/custom/modules/init.lua
+++ nvim/lua/custom/modules/init.lua
@@ -3,7 +3,8 @@
 -- ================================================================================
 
 require("custom.modules.eof_padding")
-require("custom.modules.code_runner")
+require("custom.modules.runner")
+require("custom.modules.debugger")
 require("custom.modules.window_swapper")
 require("custom.modules.winbar_breadcrumbs")
 require("custom.modules.divider_generator")
